0866c8b0b7ee517be1eb77f78795705ae6ccc8be
xpehter <xpeh18@ya.ru>
2018-10-17 23:33:32 +0500

    Initial commit

---
+++ Test.py
@@ -0,0 +1,16 @@
+import subprocess
+
+command = ['git']
+exec_command = subprocess.Popen(command, stdout=subprocess.PIPE)
+# data = exec_command.communicate()
+# print(data)
+
+# for line in data:
+#     print(line)
+
+# for line in exec_command.stdout:
+#     print(">>> " + str(line.rstrip()))
+#     exec_command.stdout.flush()
+
+for line in iter(exec_command.stdout.readline(), b''):
+    print(">>> " + line.rstrip())
